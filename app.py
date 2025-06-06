f1856d05f8cb37489873dd7b7af714b350767bb5
nerdyboyy <nerdyboyy69@gmail.com>
2025-06-06 13:05:28 +0500

    ready to deploy v1

--- app.py
+++ app.py
@@ -45,6 +45,6 @@ async def main(file: UploadFile= File(...))-> Dict[str, Any]:
     return {"data":data}
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="localhost", port=8000)
 
 
